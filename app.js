68a8ba1a78d63800737b3c5c84f901fb77a16434
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-22 11:01:57 -0700

    beanie list function to create beanie card as object written and passed to display in appjs

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 // import services and utilities
 
 // import component creators
-
+import createBeanieList from './componets/BeanieList.js';
 // declare state variables
-
+let beanies = [];
 // write handler functions
 
 // Create each component: 
+const BeanieList = createBeanieList(document.querySelector('#beanie-list'));
 // - pass in the root element via querySelector
 // - pass any needed handler functions as properties of an actions object 
 
 // Roll-up display function that renders (calls with state) each component
+
 function display() {
     // Call each component passing in props that are the pieces of state this component needs
+    BeanieList({ beanies });
 }
 
 // Call display on page load
---
+++ components/BeanieList.js
@@ -0,0 +1,29 @@
+//ask Dane or Andrew for clarification to better understand this function
+export default function createBeanieList(root) {
+    return ({ beanies }) => {
+        root.innerHTML = '';
+        for (const beanie of beanies) {
+            const li = BeanieCard({ beanie });
+            root.append(li);
+        }
+    };
+}
+
+export function BeanieCard({ beanie }) {
+    const li = document.createElement('li');
+    //'a' tag in html is for a link
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    const p = document.createElement('p');
+    //adds class to the creates li element
+    li.classList.add = ('beanie-card');
+    //point the created element contents towards the information on supabase
+    p.textContent = beanie.title;
+    img.src = beanie.image;
+    a.href = beanie.link;
+
+    a.append(p, img);
+    li.append(a);
+
+    return li;
+}
