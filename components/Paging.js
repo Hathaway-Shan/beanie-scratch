72123c83082a86f4449145eb968564b2f627092d
Hathaway-Shan <hathaway.shan@gmail.com>
2022-06-24 12:30:07 -0700

    wrote a handle paging function not really sure why but I did

---
+++ components/Paging.js
@@ -0,0 +1,26 @@
+export default function createPaging(root, { handlePaging }) {
+    const selectSize = root.querySelector('select');
+    const pageInfo = root.querySelector('.page-info');
+    const [prev, next] = root.querySelector('button');
+
+
+    prev.addEventListener('click', () => {
+        handlePaging(-1, selectSize.value);
+    });
+
+    next.addEventListener('click', () => {
+        handlePaging(1, selectSize.value);
+    });
+
+    selectSize.addEventListener('click', () => {
+        handlePaging(0, selectSize.value);
+    });
+
+    return ({ page, pageSize, totalPages }) => {
+        selectSize.value = pageSize;
+        prev.disabled = page === 1;
+        next.disabled = page === totalPages;
+
+        pageInfo.textContent = `Page ${page} of ${totalPages}`;
+    };
+}
